36a56eb8e913e7225ebf64263859f048e52d7dcc
Sherrif <msherrif04@gmail.com>
2025-11-15 11:49:41 +0000

    django-orm

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, OrderItem, Order
 
 # from django.http import HttpResponse
 
 
 # Create your views here.
 def say_hello(request):
-    query_set = Product.objects.all()
-    return render(request, "hello.html", context={"name": "Frontera"})
+    # select_related when the other end of the relations has one instance
+    # prefetch_related when the other end has many relations
+    # queryset = Product.objects.prefetch_related("promotions").select_related(
+    #     "collection"
+    # )
+    queryset = (
+        Order.objects.prefetch_related("orderitem_set__product")
+        .order_by("-placed_at")[:5]
+        .select_related("customer")
+    )
+
+    return render(
+        request,
+        "hello.html",
+        context={"name": "Frontera", "orders": list(queryset)},
+    )
